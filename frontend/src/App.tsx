d7208f30d4a2a0640cc8cb9fe4d7b5eac5f985e3
George <georgybocharovfrtc@gmail.com>
2026-08-13 00:56:25 +0300

    Review: фикс проверки pkce для frontend добавление airflow коннекто через енв переменные

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,9 +11,17 @@ const keycloakConfig: KeycloakConfig = {
 
 const keycloak = new Keycloak(keycloakConfig);
 
+const initOptions = {
+  onLoad: 'login-required',
+  pkceMethod: 'S256'       
+};
+
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider
+        authClient={keycloak}
+        initOptions={initOptions}
+    >
       <div className="App">
         <ReportPage />
       </div>
